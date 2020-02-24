683e96a6f6c87d0edd811c85d8c3811923f20382
Andrew Miller <amiller@schospital.local>
2020-02-24 11:24:10 -0600

    update

--- js/position-privacy.js
+++ js/position-privacy.js
@@ -22,5 +22,25 @@ function position(){
     else{
         $('#nav-icon-tr').addClass('hidden');
     }
+
+    if($(window).width() > 1197 && $(window).height() < 890){
+        $('#f3').css('width', '50%');
+    }
+    else{
+        $('#f3').css('width', '100%');
+    }
+
+    /*let colOffset = $('#left-col').offset().top + $('#left-col').height();
+
+    if(colOffset > $(window).height()){
+        let diff = colOffset - $(window).height();
+        let f3Height = $('#f3').height();
+        let f3NewHeight = f3Height - diff - 60
+        $('#f3').css('height', f3NewHeight);
+        $('#f3').css('width', f3NewHeight * 444 / 488);
+    }
+    else{
+        //$('#f3').css('width', '100%');
+    }*/
     
 }
